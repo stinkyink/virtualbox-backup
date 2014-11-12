e73b44e917d0ff6202b022f03698aac1ceaa802c
Paul Gideon Dann <pdgiddie@gmail.com>
2014-11-12 16:35:46 +0000

    Determine parents of hard disk images using method that works with older VirtualBox

--- virtual_hard_disk.rb
+++ virtual_hard_disk.rb
@@ -8,7 +8,7 @@ class VirtualHardDisk
   end
 
   def parent
-    parent_uuid = info['Parent UUID']
+    parent_uuid = parent_uuids[@uuid]
     if parent_uuid.nil?
       fail("No parent UUID found for HDD #{@uuid}")
     end
@@ -37,6 +37,23 @@ class VirtualHardDisk
   end
 
   private
+
+  def parent_uuids
+    return @@parent_uuids  if defined?(@@parent_uuids)
+    @@parent_uuids = Hash.new
+    current_uuid = nil
+    `#{VBOX_MANAGE} list hdds`.split("\n").each do |line|
+      key, value = line.split(':').map(&:strip)
+      current_uuid = value  if key == 'UUID'
+      if key == 'Parent UUID'
+        @@parent_uuids[current_uuid] = value
+      end
+    end
+    if $?.exitstatus != 0
+      fail 'Failed to get list of virtual hard disks'
+    end
+    @@parent_uuids
+  end
 
   def dvd_uuids
     @@dvd_uuids ||=
