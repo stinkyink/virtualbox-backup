ce0bdfee43bf911bfbdbdaa4275514d79f3e20f7
Paul Gideon Dann <pdgiddie@gmail.com>
2014-11-19 16:52:11 +0000

    Add support for pushing to Amazon S3 as well as Glacier

--- lib/offsite_backup.rb
+++ lib/offsite_backup.rb
@@ -7,15 +7,45 @@ class OffsiteBackup
   end
 
   def push_offsite!
-    push_to_amazon
+    backend = CONFIG['offsite-backend']
+    case backend
+    when 'test'
+      push_to_test_file!
+    when 'glacier'
+      push_to_glacier!
+    when 's3'
+      push_to_s3!
+    else
+      fail "Unknown offsite backend: #{backend}"
+    end
   rescue ProcessingError => e
     STDERR.puts "ERROR: #{@vm}: #{e.message}"
   end
 
   private
 
-  def push_to_amazon
-    say "== Pushing #{@local_dir} to Amazon"
+  def push_to_test_file!
+    say "== Pushing #{@local_dir} to Test File"
+    with_offsite_data_io do |data_in|
+      push_io_into_test_file(data_in)
+    end
+  end
+
+  def push_to_glacier!
+    say "== Pushing #{@local_dir} to Amazon Glacier"
+    with_offsite_data_io do |data_in|
+      push_io_into_glacier_archive(data_in)
+    end
+  end
+
+  def push_to_s3!
+    say "== Pushing #{@local_dir} to Amazon S3"
+    with_offsite_data_io do |data_in|
+      push_io_into_s3_file(data_in)
+    end
+  end
+
+  def with_offsite_data_io(&block)
     tar_cmd = %(tar c -C "#{File.dirname(@local_dir)}" ) +
               %("#{File.basename(@local_dir)}")
     unless $options.quiet
@@ -32,11 +62,10 @@ class OffsiteBackup
       IO.pipe do |data_out, data_in|
         pid = Process.spawn(cmd, out: data_in)
         data_in.close
-        push_io_into_glacier_archive(data_out)
-        # push_io_into_test_file(data_out)
+        yield(data_out)
         Process.wait(pid)
         if $?.exitstatus != 0
-          fail 'Failed to push to Amazon'
+          fail 'Failed to read backup data to push offsite'
         end
       end
     end
@@ -45,7 +74,7 @@ class OffsiteBackup
   def push_io_into_test_file(io_in)
     io_out = File.open(File.join(SCRIPT_DIR, 'test.out'), 'w')
     while true do
-      buffer = io_in.read(CONFIG['aws']['glacier']['upload-chunk-size'])
+      buffer = io_in.read(CONFIG['aws']['upload-chunk-size'])
       break  if buffer.nil?
       io_out.write(buffer)
     end
@@ -66,12 +95,28 @@ class OffsiteBackup
         create(description: @description,
                body: io_in,
                multipart_chunk_size:
-                 CONFIG['aws']['glacier']['upload-chunk-size'])
+                 CONFIG['aws']['upload-chunk-size'])
     say "Glacier archive: #{@description}"
     say "Glacier archive ID: #{archive.id}"
     CSV.open(CONFIG['local']['archive-list-file'], 'a') do |log_out|
       log_out << [@description, archive.id]
     end
+  end
+
+  def push_io_into_s3_file(io_in)
+    # patch io so that #pos= is a null-op, as Excon attempts to call it
+    def io_in.rewind
+    end
+    s3 = Fog::Storage::AWS.new({
+      aws_access_key_id: CONFIG['aws']['access-key'],
+      aws_secret_access_key: CONFIG['aws']['secret-key'],
+      region: CONFIG['aws']['region']
+    })
+    bucket = s3.directories.get(CONFIG['aws']['s3']['bucket'])
+    bucket.files.
+      create(key: @description,
+             body: io_in,
+             multipart_chunk_size: CONFIG['aws']['upload-chunk-size'])
   end
 
   def directory_size(path)
